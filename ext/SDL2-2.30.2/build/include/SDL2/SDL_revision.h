c16cb2a57506a25e061222e6325c83907068530c
Matthew Walters <mw2gd@virginia.edu>
2024-04-13 16:34:33 -0400

    removed build files from sdl2 folder

--- ext/SDL2-2.30.2/build/include/SDL2/SDL_revision.h
+++
@@ -1,8 +0,0 @@
-/* #undef SDL_VENDOR_INFO */
-#define SDL_REVISION_NUMBER 0
-
-#ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-release-2.30.2-0-gf461d91cd (" SDL_VENDOR_INFO ")"
-#else
-#define SDL_REVISION "SDL-release-2.30.2-0-gf461d91cd"
-#endif
